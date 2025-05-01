93d44edeb467e9c8e13b234e66aea9a0493b23d5
Hanyang Xu <hanyangxu709@gmail.com>
2025-05-01 16:21:46 -0500

    with github token

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import { getInput } from "@actions/core"
+import * as core from "@actions/core";
 import { getOctokit, context } from "@actions/github";
 
 async function run() {
-    const required_fields = getInput("required_fields")
-
+    const required_fields = core.getInput("required_fields")
     console.log("hello! " + required_fields)
+
+    const token = core.getInput("github-token", { required: true });
+    const octokit = getOctokit(token);
+
+    const payload = context.payload;
+
+    const senderInfo = payload?.sender;
+    const senderName = senderInfo?.login;
+    const senderType = senderInfo?.type;
+
+    console.log("senderInfo: ", senderInfo);
+    console.log("senderName: ", senderName);
+    console.log("senderType: ", senderType);
+
+    core.info(`PR created by ${senderName} (${senderType})`)
 }
 
 run()
